f00b5285d6f2770cf45168abf5505510916deb6e
Douglas <douglasrizzom@gmail.com>
2017-08-17 18:25:03 -0300

    structured old tests

--- test/main.cpp
+++ test/main.cpp
@@ -24,61 +24,80 @@ double fd(double x, double y) {
   return pow((1 - y), 2) + 100 * pow((x - pow(y, 2)), 2);
 }
 
-int main() {
-  cout.precision(12);
-  double x = 2, y = 2, error = 1e-8, result;
-  int iters = 1000;
-  tuple<double, double> d2_ret;
-  int learnRateFraction = 100;
-  Optimizer o;
+//! \param x
+//! \return e^x
+double fe(double x) { return exp(x); }
 
-  cout << "Finding roots of x^2...\n";
+//! \param x
+//! \return sqrt(1 - pow(x, 2))
+double ff(double x) { return sqrt(1 - pow(x, 2)); }
+
+//! \param x
+//! \return exp(-x^2)
+double fg(double x) { return exp(-(x*x)); }
+
+void testRoots(double x, double error, double result, int iters,
+               tuple<double, double> &d2_ret, int learnRateFraction,
+               Optimizer &o) {
   for (int i = 1; i <= learnRateFraction; i ++) {
     double learnRate = (double) i / learnRateFraction;
     result = o.findRoot(fa, x, error, iters, learnRate);
-    cout << "Beta = " << learnRate << ": x = " << result
-         << " (0), " << o.getIterations() << " iterations, error = " << o.getError() << ", end reason: "
-         << o.getEndReason() << "\n";
+    cout << "Root,1," << learnRate << "," << result << "," << o.getIterations()
+         << "," << o.getError() << "," << o.getEndReason() << "\n";
   }
-  cout << "\nFinding roots of x^3 - 2x^2 + 2...\n";
   for (int i = 1; i <= learnRateFraction; i ++) {
     double learnRate = (double) i / learnRateFraction;
     result = o.findRoot(fb, x, error, iters, learnRate);
-    cout << "Beta = " << learnRate << ": x = " << result
-         << " (-0.83929), " << o.getIterations() << " iterations, error = " << o.getError() << ", end reason: "
-         << o.getEndReason() << "\n";
+    cout << "Root,2," << learnRate << "," << result << "," << o.getIterations()
+         << "," << o.getError() << "," << o.getEndReason() << "\n";
   }
-  cout << "\nFinding minimum of x^2...\n";
+}
+
+void testMinimization(double x, double y, double error, double result,
+                      int iters, tuple<double, double> &d2_ret,
+                      int learnRateFraction, Optimizer &o) {
   for (int i = 1; i <= learnRateFraction; i ++) {
     double learnRate = (double) i / learnRateFraction;
     result = o.minimize(fa, x, error, iters, learnRate);
-    cout << "Beta = " << learnRate << ": x = " << result
-         << " (0), " << o.getIterations() << " iterations, error = " << o.getError() << ", end reason: "
-         << o.getEndReason() << "\n";
+    cout << "Minimum,1," << learnRate << "," << result << ","
+         << o.getIterations() << "," << o.getError() << "," << o.getEndReason()
+         << "\n";
   }
-  cout << "\nFinding minimum of x^3 - 2x^2 + 2...\n";
   for (int i = 1; i <= learnRateFraction; i ++) {
     double learnRate = (double) i / learnRateFraction;
-    result = o.findRoot(fa, x, error, iters, learnRate);
-    cout << "Beta = " << learnRate << ": x = " << result
-         << " (" << 4.0 / 3.0 << "), " << o.getIterations() << " iterations, error = " << o.getError()
-         << ", end reason: " << o.getEndReason() << "\n";
+    result = o.minimize(fb, x, error, iters, learnRate);
+    cout << "Minimum,2," << learnRate << "," << result << ","
+         << o.getIterations() << "," << o.getError() << "," << o.getEndReason()
+         << "\n";
   }
-  cout << "\nFinding minimum of (1 - x)^2 + (1 - y)^2...\n";
   for (int i = 1; i <= learnRateFraction; i ++) {
     double learnRate = (double) i / learnRateFraction;
     d2_ret = o.minimize(fc, x, y, error, iters, learnRate);
-    cout << "Beta = " << learnRate << ": x = " << '(' << get<0>(d2_ret) << ", " << get<1>(d2_ret) << ") (1, 1), "
-         << o.getIterations() << " iterations, error = " << o.getError() << ", end reason: " << o.getEndReason()
-         << "\n";
+    cout << "Minimum,3," << learnRate << "," << get<0>(d2_ret) << ", "
+         << get<1>(d2_ret) << "," << o.getIterations() << "," << o.getError()
+         << "," << o.getEndReason() << "\n";
   }
-  cout << "\nFinding minimum of (1 - y)^2 + 100(x - y^2)^2...\n";
   for (int i = 1; i <= learnRateFraction; i ++) {
     double learnRate = (double) i / learnRateFraction;
     d2_ret = o.minimize(fd, x, y, error, iters, learnRate);
-    cout << "Beta = " << learnRate << ": x = " << '(' << get<0>(d2_ret) << ", " << get<1>(d2_ret)
-         << ") (1, 1), " << o.getIterations() << " iterations, error = " << o.getError() << ", end reason: "
-         << o.getEndReason() << "\n";
+    cout << "Minimum,4," << learnRate << "," << get<0>(d2_ret) << ", "
+         << get<1>(d2_ret) << "," << o.getIterations() << "," << o.getError()
+         << "," << o.getEndReason() << "\n";
+  }
+}
+
+int main() {
+  cout.precision(12);
+  double x = 2, y = 2, error = 1e-50, result, low = 0, high = 1;
+  int iters = 1000000;
+  tuple<double, double> d2_ret;
+  int learnRateFraction = 100;
+  int quadratures = 200;
+  Optimizer o;
+
+  //  testRoots(x, error, result, iters, d2_ret, learnRateFraction, o);
+  //  testMinimization(x, y, error, result, iters, d2_ret, learnRateFraction, o);
+
   }
 
   return 0;
